76594774ab60833668f27eac9b1e4fa24f9b5321
M.C. van den Boogaart <scola@kvdb.name>
2018-12-10 15:08:11 +0100

    Accept object in ufw

--- src/helper/ufw.js
+++ src/helper/ufw.js
@@ -1,3 +1,3 @@
-export default function ufw(port, act = 'allow', dir = 'in', on = 'eth0', proto = 'tcp', from = 'any', to = 'any') {
+export default function ufw({ port, act = 'allow', dir = 'in', on = 'eth0', proto = 'tcp', from = 'any', to = 'any' }) {
   return `ufw ${act} ${dir} on ${on} from ${from} to ${to} port ${port} proto ${proto}`;
 }
