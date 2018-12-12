c47c70d5d934c0b8b319b34fa66a3b23abdd4990
M.C. van den Boogaart <scola@kvdb.name>
2018-12-12 22:49:52 +0100

    Fix special chars in copy

--- src/helper/copy.js
+++ src/helper/copy.js
@@ -1,5 +1,11 @@
 import { readFileSync } from 'fs';
+import sprintf from 'sprintf-js';
 
-export default function copy(local, remote) {
-  return `printf "${readFileSync(local)}" | sudo tee ${remote}`;
+export default function copy(local, remote, object = {}) {
+  local = String(readFileSync(local));
+  local = local.replace(/"/g, '\\"');
+  local = local.replace(/\$/g, '\\$');
+  local = sprintf.sprintf(local, object);
+
+  return `printf "${local}" | sudo tee ${remote}`;
 }
