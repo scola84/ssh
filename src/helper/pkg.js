6e40bf620a076ab73dc9e5ae30099e5f09effb1e
M.C. van den Boogaart <scola@kvdb.name>
2019-01-21 23:11:17 +0100

    Fix pkg silence

--- src/helper/pkg.js
+++ src/helper/pkg.js
@@ -1,4 +1,4 @@
 export default function pkg(action, name = '', version = '') {
   version = version ? '=' + version : '';
-  return `DEBIAN_FRONTEND=noninteractive apt-get -qq ${action} ${name}${version}`;
+  return `DEBIAN_FRONTEND=noninteractive apt-get -yq ${action} ${name}${version}`;
 }
